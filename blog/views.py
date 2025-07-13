6c28e78d915beb8330f1e8899cc997af6a58c267
Tom Bragg <tbragg88@hotmail.com>
2025-07-13 13:16:19 +0100

    Refactor code for consistency and readability across multiple files

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
-# Create your views here.
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 3
+    paginate_by = 6
